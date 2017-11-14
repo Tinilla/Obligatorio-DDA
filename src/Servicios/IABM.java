ffe18109eb2e87772924d2b0e6f15370e056cfdf
Equipo <Equipo@DESKTOP-5SA0ARI>
2017-11-13 21:22:15 -0300

    Merge origin/Hola

---
+++ src/Servicios/IABM.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servicios;
+
+/**
+ *
+ * @author Equipo
+ */
+public interface IABM<T> {
+    boolean alta(T objeto);
+    boolean baja (int id);
+    boolean modificar(T objeto);
+}
